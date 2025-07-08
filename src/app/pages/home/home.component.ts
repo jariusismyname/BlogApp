e155e0541ec43af8dc0a9b0db552f86a8f215cb8
Jarius Miguel Catapang Ballesteros <jrsmglctpngbllstrs@gmail.com>
2025-07-08 18:34:44 +0800

    Add Post Feature

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,49 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HeaderComponent,FooterComponent],
+  imports: [CommonModule, FormsModule, HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  posts: string[] = [];
+  newPost: string = '';
+  editingIndex: number | null = null;
 
+  addPost() {
+    if (this.newPost.trim()) {
+      this.posts.push(this.newPost.trim());
+      this.newPost = '';
+    }
+  }
+
+  editPost(index: number) {
+    this.editingIndex = index;
+    this.newPost = this.posts[index];
+  }
+
+  updatePost() {
+    if (this.editingIndex !== null && this.newPost.trim()) {
+      this.posts[this.editingIndex] = this.newPost.trim();
+      this.cancelEdit();
+    }
+  }
+
+  deletePost(index: number) {
+    this.posts.splice(index, 1);
+    if (this.editingIndex === index) {
+      this.cancelEdit();
+    }
+  }
+
+  cancelEdit() {
+    this.editingIndex = null;
+    this.newPost = '';
+  }
 }
-
